e62a380fb2162eb4f9250627f829581bebe557a6
moonwlsdnl <moonwlsdnl@gmail.com>
2025-05-22 02:14:41 +0000

    first_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ st.set_page_config(
     page_title='GDP dashboard',
     page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
 )
+st.title("My new Streamlit app")
 
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
